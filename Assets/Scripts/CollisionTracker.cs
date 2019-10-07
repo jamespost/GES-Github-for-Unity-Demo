5528e71fed2ec2a44e7f3395f4097b9a2c07ec34
jamespost <32137126+jamespost@users.noreply.github.com>
2019-10-07 13:46:23 -0500

    Create with Code Unit 1 assignment initial commit

---
+++ Assets/Scripts/CollisionTracker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionTracker : MonoBehaviour
+{
+    [SerializeField] Text damageText;
+    int damage = 0;
+    Material material;
+    float damageColor = 0f;
+    float damageOpacity= 0f;
+    
+
+    private void Start()
+    {
+        //get renderer component of game object
+        Renderer renderer = GetComponent<Renderer>();
+
+        //get material component of game object
+        material = renderer.material;       
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Obstacle" && damage < 100)
+        {
+            //print a collide debug log
+            Debug.Log("Obstacle Collision!");
+
+            //change the damage UI text
+            damage++;
+            damageText.text = "Damage: " + damage + " %";
+            //make the object more red in color
+            damageColor += (255 / 100f) / 10;
+            damageOpacity += (255 / 100f)/ 10;            
+            material.color = new Color(damageColor, 0f, 0f, damageOpacity);
+            Debug.Log("color is" + material.color);
+            
+        }        
+
+    }
+}
---
+++ Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefabs;
+    public int obstacleMaximum;
+    public int obstacleNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while(obstacleNumber < obstacleMaximum)
+        {
+            int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
+           
+
+            Instantiate(obstaclePrefabs[obstacleIndex], new Vector3(0, 0, Random.Range(25, 50
+                )), obstaclePrefabs[obstacleIndex].transform.rotation);
+
+            obstacleNumber++;
+        }
+        
+    }
+        // Update is called once per frame
+        void Update()
+    {
+        while (obstacleNumber < obstacleMaximum)
+        {
+            int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
+
+
+            Instantiate(obstaclePrefabs[obstacleIndex], new Vector3(0, 0, Random.Range(25, 50
+                )), obstaclePrefabs[obstacleIndex].transform.rotation);
+
+            obstacleNumber++;
+        }
+    }
+}
